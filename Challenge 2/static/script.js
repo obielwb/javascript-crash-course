02fe9b32456e8cd88e8dc4b425da7a2b93816633
Gabriel Willian <obielwb@gmail.com>
2021-05-31 16:04:18 -0300

    update static files

--- Challenge 2/static/script.js
+++ Challenge 2/static/script.js
@@ -1,6 +1,6 @@
 function generateCat() {
     var image = document.createElement("img");
     var div = document.getElementById("flex-cat-gen");
-    image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    image.setAttribute("src", "https://thecatapi.com/api/images/get?format=src&type=gif&size=small");
     div.appendChild(image);
 }
